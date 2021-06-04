1b6efc26e678198a8758e2d385fcb2e4fb72837e
songzhewen <59754885+songzhewen@users.noreply.github.com>
2021-06-04 16:05:01 +0800

    archetype示例代码

--- demo-service/src/main/java/com/szw/me/demo/enums/DemoEnum.java
+++ demo-service/src/main/java/com/szw/me/demo/enums/DemoEnum.java
@@ -1,4 +1,29 @@
 package com.szw.me.demo.enums;
 
-public enum DemoEnum {
+import lombok.Getter;
+
+@Getter
+public enum DemoResultEnum {
+
+    FAILED("-1", "失败"),
+    DUP_KEY("10001", "数据重复"),
+    NOT_FOUND("10002", "未找到数据");
+
+    private String code;
+    private String message;
+
+    DemoResultEnum(String code, String message) {
+        this.code = code;
+        this.message = message;
+    }
+
+    public static String getMessage(String code) {
+        String defaultValue = "";
+        for (DemoResultEnum demoResultEnum : DemoResultEnum.values()) {
+            if (demoResultEnum.getCode().equals(code)) {
+                return demoResultEnum.getMessage();
+            }
+        }
+        return defaultValue;
+    }
 }
