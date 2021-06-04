c775c118b67b25f083ce5dfc941c07f1c818a233
songzhewen <59754885+songzhewen@users.noreply.github.com>
2021-06-04 16:04:13 +0800

    archetype示例代码

--- demo-service/src/main/java/com/szw/me/demo/domain/Demo.java
+++ demo-service/src/main/java/com/szw/me/demo/domain/Demo.java
@@ -1,4 +1,23 @@
 package com.szw.me.demo.domain;
 
-public class Demo {
+import java.io.Serializable;
+import lombok.Data;
+
+/**
+ * @author 
+ * 
+ */
+@Data
+public class Demo implements Serializable {
+    /**
+     * 主键
+     */
+    private Integer id;
+
+    /**
+     * 姓名
+     */
+    private String name;
+
+    private static final long serialVersionUID = 1L;
 }
