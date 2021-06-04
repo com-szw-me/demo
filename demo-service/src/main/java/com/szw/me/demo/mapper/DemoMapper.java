4c39ddb15b848f8da632da5f9aa673a8f94b66a4
songzhewen <59754885+songzhewen@users.noreply.github.com>
2021-06-04 16:06:54 +0800

    archetype示例代码

--- demo-service/src/main/java/com/szw/me/demo/mapper/DemoMapper.java
+++ demo-service/src/main/java/com/szw/me/demo/mapper/DemoMapper.java
@@ -1,4 +1,22 @@
 package com.szw.me.demo.mapper;
 
+import com.szw.me.demo.domain.Demo;
+import com.szw.me.demo.dto.DemoDto;
+import org.apache.ibatis.annotations.Mapper;
+import org.springframework.stereotype.Repository;
+
+@Mapper
+@Repository
 public interface DemoMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(Demo record);
+
+    int insertSelective(Demo record);
+
+    Demo selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(Demo record);
+
+    int updateByPrimaryKey(Demo record);
 }
