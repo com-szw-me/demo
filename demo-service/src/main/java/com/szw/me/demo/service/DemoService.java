37d723b2269e77459b4150e3c0bd0e11235ba5ff
songzhewen <59754885+songzhewen@users.noreply.github.com>
2021-06-04 16:10:08 +0800

    archetype示例代码

--- demo-service/src/main/java/com/szw/me/demo/service/DemoService.java
+++ demo-service/src/main/java/com/szw/me/demo/service/DemoService.java
@@ -1,4 +1,12 @@
 package com.szw.me.demo.service;
 
+import com.szw.me.demo.req.AddDemoReq;
+import com.szw.me.demo.req.EditDemoReq;
+import com.szw.me.framework.vo.ResultVO;
+
 public interface DemoService {
+    ResultVO add(AddDemoReq req);
+    ResultVO edit(EditDemoReq req);
+    ResultVO get(Integer id);
+    ResultVO delete(Integer id);
 }
