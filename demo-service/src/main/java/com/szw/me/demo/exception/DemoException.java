2c9e68928075158998d45075c74b26dde4bfd08a
songzhewen <59754885+songzhewen@users.noreply.github.com>
2021-06-04 16:05:43 +0800

    archetype示例代码

--- demo-service/src/main/java/com/szw/me/demo/exception/DemoException.java
+++ demo-service/src/main/java/com/szw/me/demo/exception/DemoException.java
@@ -1,4 +1,23 @@
 package com.szw.me.demo.exception;
 
-public class DemoException {
+import com.szw.me.demo.enums.DemoResultEnum;
+import lombok.Getter;
+
+@Getter
+public class DemoException extends RuntimeException {
+
+    private String code;
+    private String message;
+
+    public DemoException(DemoResultEnum resultEnum) {
+        super(resultEnum.getMessage());
+        this.code = resultEnum.getCode();
+        this.message = resultEnum.getMessage();
+    }
+
+    public DemoException(String message) {
+        super(message);
+        this.code = DemoResultEnum.FAILED.getCode();
+        this.message = message;
+    }
 }
