15c0c4694250baf4ebe9798cbb3c521dd2e73db6
songzhewen <59754885+songzhewen@users.noreply.github.com>
2021-06-04 15:59:16 +0800

    archetype示例代码

--- demo-client/src/main/java/com/szw/me/demo/client/DemoClient.java
+++ demo-client/src/main/java/com/szw/me/demo/client/DemoClient.java
@@ -1,4 +1,16 @@
 package com.szw.me.demo.client;
 
-public class DemoClient {
+import com.szw.me.framework.feign.FeignConfiguration;
+import com.szw.me.framework.vo.ResultVO;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@FeignClient(name = "demo", configuration = {FeignConfiguration.class})
+@RequestMapping("/demo")
+public interface DemoClient {
+
+    @GetMapping("/{id}")
+    ResultVO get(@PathVariable("id") Integer id);
 }
